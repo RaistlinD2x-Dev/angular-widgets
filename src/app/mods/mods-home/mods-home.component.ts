627e3e13fa94be5cd6fd931100651848fd04d0aa
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-23 21:55:09 -0500

    built accordian widget

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -9,6 +9,12 @@ export class ModsHomeComponent implements OnInit {
 
   modalOpen = false;
 
+  items = [
+    { title: 'Why is the sky blue?', content: 'because it is...' },
+    { title: 'What does an orange taste like?', content: 'It tastes like an orange.' },
+    { title: 'What color is the cat?', content: 'The cat is orange.' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
